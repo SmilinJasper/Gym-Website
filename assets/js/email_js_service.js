697b4109db34dc34fb412eb05a2811d61812fd4f
SmilinJasper <smilin.jasper@yahoo.com>
2021-08-04 16:26:16 +0530

    refactor-emailjs-service-code

--- assets/js/email_js_service.js
+++ assets/js/email_js_service.js
@@ -1,24 +1,14 @@
-/* contactForm = document.getElementById("contact-form");
-
-contactForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    emailjs.sendForm("esh_fitness_email", "template_yj6a114", this)
-        .then(function() {
-            alert("Email Sent! I will get back to you soon");
-        }, function() {
-            alert("Failed to send email, report issue to me please");
-        });
-});*/
-
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     // these IDs from the previous steps
     emailjs.sendForm('esh_fitness_email', 'template_yj6a114', this)
-        .then(function() {
-            alert("Email Sent! I will get back to you soon");
-        }, function() {
-            alert("Failed to send email, report issue to me please");
-        });
+        .then(
+            () => {
+                alert("Email sent successfully! We will get back to you shortly.")
+            },
+            () => {
+                alert("Sorry, something went wrong on our server. We are working on fixing it.")
+            }
+        );
 });

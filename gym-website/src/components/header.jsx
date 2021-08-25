5195c934e145644f4a084935ac82f2d0dbc35540
SmilinJasper <smilin.jasper@yahoo.com>
2021-08-26 01:10:05 +0530

    make-header-auto-retract

--- gym-website/src/components/header.jsx
+++ gym-website/src/components/header.jsx
@@ -41,5 +41,25 @@ class Header extends Component {
         </React.Fragment> );
     }
 }
- 
+
+window.onload = () => {
+    
+    let lastScrollTopPosition = 0;
+
+    window.addEventListener("scroll", () => {
+
+        const navBarHeader = document.querySelector(".nav-bar-header");
+
+        let scrollTopPosition = window.pageYOffset || document.documentElement.scrollTopPosition;
+
+        if (scrollTopPosition < lastScrollTopPosition || !scrollTopPosition) {
+            if (navBarHeader.classList.contains("header--scrolled-down")) return navBarHeader.classList.remove("header--scrolled-down");
+        } else {
+            if (!navBarHeader.classList.contains("header--scrolled-down")) return navBarHeader.classList.add("header--scrolled-down");
+        }
+
+    lastScrollTopPosition = scrollTopPosition === 0 ? 0 : scrollTopPosition;
+    });
+}
+
 export default Header;

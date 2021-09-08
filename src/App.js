96332ba741a4cdc349ed2d5b37c42b51800fb783
SmilinJasper <smilin.jasper@yahoo.com>
2021-09-08 23:48:20 +0530

    add-client-testimonials-section-to-home

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import HomeHeroSection from './components/homeHeroSection';
 import './App.css';
 import WhatWeOfferSection from './components/whatWeOfferSection';
 import PersonalTrainingShowcaseSection from './components/personalTrainingShowcaseSection';
+import ClientTestimonialSection from './components/clientTestimonialsSection';
 
 class App extends Component {
 
   state = {  }
 
   render() { 
-    return ( <main>
-      <Header />
-      <HomeHeroSection />
-      <WhatWeOfferSection />
-      <PersonalTrainingShowcaseSection />
-    </main> );
+    return ( 
+
+      <main>
+        <Header />
+        <HomeHeroSection />
+        <WhatWeOfferSection />
+        <PersonalTrainingShowcaseSection />
+        <ClientTestimonialSection />
+      </main> 
+      
+    );
   }
 
 }

968038e05f275c2fe0e7726a3061253aad414fcf
SmilinJasper <smilin.jasper@yahoo.com>
2021-10-11 23:57:13 +0530

    make-big-screen-nav-bar-show-on-big-screens-only

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
                             <li><NavLink to='index.html'>Contact Us</NavLink></li>
                             </ul>
                             </nav>
-                        <label for='nav-toggle' className='fas fa-bars fa-2x'></label>
+                        <label for='nav-toggle' className='nav-bar-hamburger fas fa-bars fa-2x'></label>
                     </div>
 
                     <input type='checkbox' id='nav-toggle' className='nav-toggle' />
